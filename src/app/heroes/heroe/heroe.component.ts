f4b70b22b450571c6d4c61c588f3543e7d629323
RamiroMendozaSanchez <ramiromendoza1612@gmail.com>
2021-10-09 15:42:37 -0500

    Primer commit

---
+++ src/app/heroes/heroe/heroe.component.ts
@@ -0,0 +1,29 @@
+import { NumberFormatStyle } from '@angular/common';
+import { Component, NgModule } from '@angular/core';
+
+
+
+@Component({
+    selector:'app-heroe',
+    templateUrl:'heroe.component.html'
+})
+export class HeroeComponent{
+    nombre:string = 'Ironman';
+    edad:number = 45;
+
+    get nombreCapitalizado():string{
+        return this.nombre.toUpperCase();
+    }
+
+    obtenerNombre():string{
+        return `${this.nombre} - ${this.edad}`
+    }
+
+    cambiarNombre():void{
+        this.nombre = 'SpiderMan';
+    }
+
+    cambiarEdad():void{
+        this.edad = 30;
+    }
+}
